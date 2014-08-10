ebe04668a81ef0cb06216aa7787e1577223776d2
Zach Evans <zach.evans@logik.com>
2014-08-10 01:07:17 +0000

    changes from devbox over time

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 symlinks_to_make=(
-"/Users/$USER/dotfiles/bash/bashrc /Users/$USER/.bashrc"
-"/Users/$USER/dotfiles/bash/bash_profile /Users/$USER/.bash_profile"
-"/Users/$USER/dotfiles/bash/bash_aliases /Users/$USER/.bash_aliases"
-"/Users/$USER/dotfiles/tmux/tmux.conf /Users/$USER/.tmux.conf"
-"/Users/$USER/dotfiles/git/gitconfig /Users/$USER/.gitconfig"
-"/Users/$USER/dotfiles/vim/vimrc /Users/$USER/.vimrc"
-"/Users/$USER/dotfiles/vim/dotvim /Users/$USER/.vim"
+"/var/logikcull/apps/dotfiles/bash/bashrc /var/logikcull/apps/.bashrc"
+"/var/logikcull/apps/dotfiles/bash/bash_profile /var/logikcull/apps/.bash_profile"
+"/var/logikcull/apps/dotfiles/bash/bash_aliases /var/logikcull/apps/.bash_aliases"
+"/var/logikcull/apps/dotfiles/tmux/tmux.conf /var/logikcull/apps/.tmux.conf"
+"/var/logikcull/apps/dotfiles/git/gitconfig /var/logikcull/apps/.gitconfig"
+"/var/logikcull/apps/dotfiles/vim/vimrc /var/logikcull/apps/.vimrc"
+"/var/logikcull/apps/dotfiles/vim/dotvim /var/logikcull/apps/.vim"
 )
 
 printf "===> Creating symlinks...\n"
